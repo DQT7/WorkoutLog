850d66b8582d8d60c96f20d125e938748fc15c34
DeQuon Turner <dqturner@gmail.com>
2018-01-30 10:31:17 -0500

    the rest

--- workoutlogserver/app.js
+++ workoutlogserver/app.js
@@ -1,6 +1,14 @@
+
 let express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var sequelize = require('./db.js');
+var User = sequelize.import('./models/user');
+
+User.sync();
+// user.sync({force:true});
+app.use(bodyParser.json());
+
 
 app.use(require('./middleware/headers'));
 
@@ -11,80 +19,37 @@ app.listen(3000, function(){
     console.log("app is open on 3000!");
 })
 
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('workoutlog', 'postgres', 'QuintezDT7', {
-	host: 'localhost',
-	dialect: 'postgres'
-});
 
-sequelize.authenticate().then(
-	function() {
-		console.log('connected to workoutlog postgres db');
-	},
-	function(err){
-		console.log(err);
-	}
-);
-var User = sequelize define('user', {
-    username: Sequelize.STRING,
-    passwordhash: sequelize. STRING,
-});
+// var User = sequelize.define ('user', {
+//     username: Sequelize.STRING,
+//     passwordhash: Sequelize.STRING,
+// });
 
-User.sync();
-app.use(bodyParser.json());
 
-})
-app.post('/api/user', function(req, res) {
+
+
+
+app.post('/api/user', function(req, res){
+    //when we post to api user, it will want a user object in the body
     var username = req.body.user.username;
-    var pass = req.body.user.password;
-    //Need to create a user object and use sequelize to put that user into
-    //our database.
-    User.createSchema({
-        username:username,
-        passwordhash:''
-    })
-}).then(
-    //Sequelize is going to return the object it created from db.
+    var pass = req.body.user.password;	//TODO: hash this password - HASH=not human readable
 
-    function createSuccess(user){
-        res.json({
-            user: user,
-            message: 'create'
-    });
-    
-    },
-    function createError(err){
-        res.send(500, err.message);
-    }
-);
-});
-
-)};
-app.listen(3000, function(){
-    console.log("app is open on 3000!");
-    app.post('/api/user', function(req, res){
-        //when we post to api user, it will want a user object in the body
-        var username = req.body.user.username;
-        var pass = req.body.user.password;	//TODO: hash this password - HASH=not human readable
-    
-        //Match the model we create above
-        //Sequelize - take the user model and go out to the db and create this:
-        User.create({
-            username: username,
-            passwordhash: ''
+    //Match the model we create above
+    //Sequelize - take the user model and go out to the db and create this:
+    User.create({
+        username: username,
+        passwordhash: pass
     }).then(
-            //Sequelize is going to return the object it created from db.
-            function createSuccess(user){
-                //successful get thsi:
-                res.json({
-                    user: user,
-                    message: 'create'
-                });
-            },
-            function createError(err){
-                res.send(500, err.message);
-            }
-        );
-    });
-    app.listen(3000, function(){
-        console.log("app is open on 3000!");}
+        //Sequelize is going to return the object it created from db.
+        function createSuccess(user){
+            //successful get this:
+            res.json({
+                user: user,
+                message: 'create'
+            });
+        },
+        function createError(err){
+            res.send(500, err.message);
+        }
+    );
+});
