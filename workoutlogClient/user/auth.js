4753e31a0d41c6f083175916aed6b83c46041399
Chris Smola <csmola@tangogroup.com>
2018-02-06 21:16:48 -0700

    (smola-help) WIP

--- workoutlogClient/user/auth.js
+++ workoutlogClient/user/auth.js
@@ -5,12 +5,13 @@ $(function() {
              var password = $("#su_password").val();
              var user = {user:  {username: username, password: password }};
              var signup = $.ajax({
-                type: "POST", 
-                url: WorkoutLog.API_BASE + "user", 
-                data: JSON.stringify(user), 
+                type: "POST",
+                url: WorkoutLog.API_BASE + "user",
+                data: JSON.stringify(user),
                 contentType: "application/json"
              });
              signup.done(function(data) {
+                console.log('request finished');
                 if (data.sessionToken) {
                    WorkoutLog.setAuthHeader(data.sessionToken);
                    WorkoutLog.definition.fetchAll();
@@ -19,18 +20,18 @@ $(function() {
                 $("#signup-modal").modal("hide");
                 $(".disabled").removeClass("disabled");
                 $("#loginout").text("Logout");
-               
+
                 $('.nav-tabs a[href="#define"]').tab('show');
- 
+
                 $("#su_username").val("");
                 $("#su_password").val("");
              })
              .fail(function() {
                 $("#su_error").text("There was an issue with your username").show();
-               
+
              });
        },
- 
+
        login: function() {
           var username = $("#li_username").val();
           var password = $("#li_password").val();
@@ -47,23 +48,23 @@ $(function() {
                 WorkoutLog.definition.fetchAll();
                 WorkoutLog.log.fetchAll();
              }
-             // TODO: add logic to set user and auth token  
- 
- 
+             // TODO: add logic to set user and auth token
+
+
              $("#login-modal").modal("hide");
              $(".disabled").removeClass("disabled");
              $("#loginout").text("Logout");
              $("#li_username").val("");
              $("#li_password").val("");
- 
+
              $('a[href="#define"]').tab("show");
- 
+
           })
           .fail(function() {
              $("#li_error").text("There was an issue with your username or password").show();
              });
        },
- 
+
        loginout: function() {
           if (window.localStorage.getItem("sessionToken")) {
              window.localStorage.removeItem("sessionToken");
@@ -71,14 +72,14 @@ $(function() {
           }
        }
     });
- 
+
     // bind events
     $("#login").on("click", WorkoutLog.login);
     $("#signup").on("click", WorkoutLog.signup);
     $("#loginout").on("click", WorkoutLog.loginout);
- 
+
     if (window.localStorage.getItem("sessionToken")) {
        $("#loginout").text("Logout");
     }
- 
+
  });
