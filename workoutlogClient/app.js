79fffe4143824d3ab10918e65ad0750ca2478aeb
Chris Smola <csmola@tangogroup.com>
2018-02-12 10:57:26 -0700

    (smola-help) adds missing env var

--- workoutlogClient/app.js
+++ workoutlogClient/app.js
@@ -1,239 +1,80 @@
 $(function(){
 
-    var WorkoutLog = (function($, undefined) {
-      var API_BASE =  "http://localhost:3000/api/";
-      var userDefinitions = [];
+  var WorkoutLog = (function($, undefined) {
+    var API_BASE =  "http://localhost:3000/api/";
+    var userDefinitions = [];
 
-      var setAuthHeader = function(sessionToken) {
-         window.localStorage.setItem("sessionToken", sessionToken);
-         // Set the authorization header
-         // This can be done on individual calls
-         // here we showcase ajaxSetup as a global tool
-         $.ajaxSetup({
-            // contentType: 'application/x-www-form-urlencoded',
-            headers: {
-               "Authorization": sessionToken,
-               Accept: 'application/json'
-            }
-         });
-      };
+    var setAuthHeader = function(sessionToken) {
+      window.localStorage.setItem("sessionToken", sessionToken);
+      // Set the authorization header
+      // This can be done on individual calls
+      // here we showcase ajaxSetup as a global tool
+      $.ajaxSetup({
+        // contentType: 'application/x-www-form-urlencoded',
+        headers: {
+          "Authorization": sessionToken,
+          Accept: 'application/json'
+        }
+      });
+    };
 
-      // public
-      return {
-         API_BASE: API_BASE,
-         setAuthHeader: setAuthHeader
-      };
-    })(jQuery);
+    // public
+    return {
+      API_BASE: API_BASE,
+      setAuthHeader: setAuthHeader
+    };
+  })(jQuery);
 
-      {
-        key: 'value',
-        anotherKey: true
+  // Ensure .disabled aren't clickable
+  $(".nav-tabs a[data-toggle=tab]").on("click", function(evt) {
+    var token = window.localStorage.getItem("sessionToken");
+    if ($(this).hasClass("disabled") && !token) {
+      evt.preventDefault();
+      return false;
+    }
+  });
+
+  // bind tab change events
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (evt) {
+    var target = $(evt.target).attr("href"); // activated tab
+    if (target === "#log") {
+      WorkoutLog.log.setDefinitions();
+    }
+
+    if (target === "#history") {
+      WorkoutLog.log.setHistory();
+    }
+  });
+
+  // bind enter key
+  $(document).on("keypress", function(evt) {
+    if (evt.which === 13) { // enter key
+      if ($("#signup-modal").is(":visible")) {
+        $("#signup").trigger("click");
       }
+      if ($("#login-modal").is(":visible")) {
+        $("#login").trigger("click");
+      }
+    }
+  });
 
-       // Ensure .disabled aren't clickable
-       $(".nav-tabs a[data-toggle=tab]").on("click", function(evt) {
-          var token = window.localStorage.getItem("sessionToken");
-          if ($(this).hasClass("disabled") && !token) {
-             e.preventDefault();
-             return false;
-          }
-       });
+  // click button to close modal
+  $('#login').on("click", function(evt) {
+    if ($("#signup-modal").is(":visible")) {
+      $("#signup").trigger("click");
+    }
+    if ($("#login-modal").is(":visible")) {
+      $("#login").trigger("click");
+    }
+  });
 
-       //bind tab change events
-       $('a[data-toggle="tab"]').on('shown.bs.tab', function (evt) {
-          var target = $(e.target).attr("href"); // activated tab
-          if (target === "#log") {
-             WorkoutLog.log.setDefinitions();
-          }
+  // setHeader if we
+  var token = window.localStorage.getItem("sessionToken");
+  if (token) {
+    WorkoutLog.setAuthHeader(token);
+  }
 
-          if (target === "#history") {
-             WorkoutLog.log.setHistory();
-          }
-       });
-
-       // bind enter key
-       $(document).on("keypress", function(evt) {
-          if (e.which === 13) { // enter key
-             if ($("#signup-modal").is(":visible")) {
-                $("#signup").trigger("click");
-             }
-             if ($("#login-modal").is(":visible")) {
-                $("#login").trigger("click");
-             }
-          }
-       });
-
-       // bind tab change events
-      // bootstrap tab --> binding to a bootstrap event
-       $('a[data-toggle="tab"]').on('shown.bs.tab', function (evt) {
-          var target = $(e.target).attr("href"); // activated tab
-          if (target === "#log") {
-             WorkoutLog.log.setDefinitions();
-          }
-
-          if (target === "#history") {
-             WorkoutLog.log.setHistory();
-          }
-       });
-
-       // click button to close modal
-       $('#login').on("click", function(evt) {
-          if ($("#signup-modal").is(":visible")) {
-            $("#signup").trigger("click");
-          }
-          if ($("#login-modal").is(":visible")) {
-            $("#login").trigger("click");
-          }
-       });
-
-       // setHeader if we
-       var token = window.localStorage.getItem("sessionToken");
-       if (token) {
-          WorkoutLog.setAuthHeader(token);
-       }
-
-       // expose this to the other workoutlog modules
-       window.WorkoutLog = WorkoutLog;
-    });
-
-
-
-
-    // $(function(){
-
-
-
-    // });
-
-    //step 3
-    // $(function(){
-
-    // var WorkoutLog = (function($, undefined) {
-
-    //    })(jQuery);
-
-
-    // });
-
-
-
-    //Step 4
-    // $(function(){
-
-    // var WorkoutLog = (function($, undefined) {
-    //       var API_BASE =  "http://localhost:3000/api/";
-    //       var userDefinitions = [];
-
-
-    //     })(jQuery);
-
-
-    // });
-
-    //Step 5
-
-    // $(function(){
-
-    // var WorkoutLog = (function($, undefined) {
-    //       var API_BASE =  "http://localhost:3000/api/";
-    //       var userDefinitions = [];
-
-    //       var setAuthHeader = function(sessionToken) {
-    //          window.localStorage.setItem("sessionToken", sessionToken);
-    //          // Set the authorization header
-    //          // This can be done on individual calls
-    //          // here we showcase ajaxSetup as a global tool
-    //          $.ajaxSetup({
-    //             "headers": {
-    //                "Authorization": sessionToken
-    //             }
-    //          });
-    //       };
-
-    //    })(jQuery);
-
-
-
-    // });
-
-    //STep 6
-    // $(function(){
-
-    // var WorkoutLog = (function($, undefined) {
-    //       var API_BASE =  "http://localhost:3000/api/";
-    //       var userDefinitions = [];
-
-    //       var setAuthHeader = function(sessionToken) {
-    //          window.localStorage.setItem("sessionToken", sessionToken);
-    //          // Set the authorization header
-    //          // This can be done on individual calls
-    //          // here we showcase ajaxSetup as a global tool
-    //          $.ajaxSetup({
-    //             "headers": {
-    //                "Authorization": sessionToken
-    //             }
-    //          });
-    //       };
-
-    //       // public
-    //       return {
-    //          API_BASE: API_BASE,
-    //          setAuthHeader: setAuthHeader
-    //       };
-    //    })(jQuery);
-
-
-
-    // });
-
-
-    //Step 7 is code up until now.
-
-    //step 8
-      // })(jQuery);
-
-      //  // Ensure .disabled aren't clickable
-      //  $(".nav-tabs a[data-toggle=tab]").on("click", function(evt) {
-      //     var token = window.localStorage.getItem("sessionToken");
-      //     if ($(this).hasClass("disabled") && !token) {
-      //        e.preventDefault();
-      //        return false;
-      //     }
-      //  });
-
-
-    //step 9
-       // bind tab change events
-       // $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-       //    var target = $(e.target).attr("href"); // activated tab
-       //    if (target === "#log") {
-       //       WorkoutLog.log.setDefinitions();
-       //    }
-
-       //    if (target === "#history") {
-       //       WorkoutLog.log.setHistory();
-       //    }
-       // });
-
-    //step 10
-       // bind enter key
-       // $(document).on("keypress", function(e) {
-       //    if (e.which === 13) { // enter key
-       //       if ($("#signup-modal").is(":visible")) {
-       //          $("#signup").trigger("click");
-       //       }
-       //       if ($("#login-modal").is(":visible")) {
-       //          $("#login").trigger("click");
-       //       }
-       //    }
-       // });
-
-    //step 11
-       // setHeader if we
-       // var token = window.localStorage.getItem("sessionToken");
-       // if (token) {
-       //    WorkoutLog.setAuthHeader(token);
-       // }
-
-       // // expose this to the other workoutlog modules
-       // window.WorkoutLog = WorkoutLog;
+  // expose this to the other workoutlog modules
+  window.WorkoutLog = WorkoutLog;
+  console.log('setting window object: %o', window.WorkoutLog);
+});
--- workoutlogserver/routes/user.js
+++ workoutlogserver/routes/user.js
@@ -4,26 +4,25 @@ var User = sequelize.import('../models/user');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
-router.post('/', function(req, res){
-
-    var username = req.body.user.username;
-    var pass = req.body.user.password; //TODO: hash this pw
-    User.create({
-        username: username,
-        passwordhash:bcrypt.hashSync(pass,10) //TODO: make it hashed
-    }).then(
-        function createSuccess(user){
-            var token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
-            res.json({
-                user: user,
-                message: 'created',
-                sessionToken: token
-            });
-        },
-        function createError(err){
-            res.send(500, err.message);
-        }
-    );	
+router.post('/', function(req, res) {
+  var username = req.body.user.username;
+  var pass = req.body.user.password; //TODO: hash this pw
+  User.create({
+    username: username,
+    passwordhash: bcrypt.hashSync(pass,10) //TODO: make it hashed
+  }).then(
+    function createSuccess(user) {
+      var token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60*60*24 });
+      res.json({
+        user: user,
+        message: 'created',
+        sessionToken: token
+      });
+    },
+    function createError(err) {
+      res.send(500, err.message);
+    }
+    );
 });
 
 module.exports = router;
